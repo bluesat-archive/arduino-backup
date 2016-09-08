cd2631f845cd697a480402f82c830e94cd228b1f
Sebastian Holzapfel <sebastian.holzapfel@nicta.com.au>
2016-09-09 00:54:18 +1000

    fix build

--- arduino/CMakeFiles/arduino-upload.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/arduino-upload"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/arduino-upload.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- arduino/CMakeFiles/uno_Adafruit_PWMServoDriver.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libuno_Adafruit_PWMServoDriver.a"
-)
