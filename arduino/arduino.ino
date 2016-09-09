1f4af52437334ac50f77f9c9d680ebc22ae2b945
helenaKertesz <helena.kertesz1@gmail.com>
2016-09-09 17:28:36 +1000

    Adding lines of code. Will fix in a bit

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -18,6 +18,26 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 char buffer[sizeof(toControlMsg)];
 char bytesRead;
 
+
+/********** COPY FROM CLAW FEEDBACK ******************/
+
+#define NUMBER -70
+#define OTHER_NUMBER 70
+
+
+int out = 0;    // variable to store the servo position
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int servoPin = 9;
+int count = 0;
+int avg_pos = 0;
+int avg_error = 0;
+int clawCommand = 0;
+int32_t frequency = 50;
+
+
+/*****************************************************/
+
+
 void setup() {
     //Serial.begin(19200); //this is the speed specified on line 109 of Bluetongue.cpp I think it is correct
 
@@ -25,8 +45,13 @@ void setup() {
 
     bytesRead = 0;
     pwm.begin();
-  
+
     pwm.setPWMFreq(52);
+
+/********** COPY FROM CLAW FEEDBACK ******************/
+   yield();
+/*****************************************************/
+
 }
 bool foundFirst = false;
 toMsgAdapter msg;
@@ -56,10 +81,63 @@ void loop() {
           }
         }
     }
-    
 
     //TODO: add adc code here
+
     //TODO: add claw feeedback here
+    /********** COPY FROM CLAW FEEDBACK ******************/
+    float sensorValue = 0;        // value read from the pot
+    sensorValue = analogRead(analogInPin);
+    sensorValue = sensorValue * 2.9412 + 400;
+
+    /*Serial.print("Sensor Value = ");
+      Serial.print(sensorValue);
+      Serial.print("\n");*/
+
+    int clawActual = (int)sensorValue;
+    avg_pos += clawActual;
+
+    // recalibrate
+
+
+
+    if (Serial.available() > 0) {
+       clawCommand = Serial.parseInt();
+       Serial.print("READ INPUT\n");
+    }
+
+    if(count == 10) {
+       avg_pos /= count;
+
+       // send back avg_pos;
+       int error_out = clawCommand - avg_pos;
+       if(error_out < NUMBER || error_out > OTHER_NUMBER) {
+          error_out = 0;
+          out = 0;
+          Serial.print("ERROR TOO BIG\n");
+       } else {
+          double out_inst = (double)(0.5*(double)(error_out) + avg_pos); // need to understand this
+          out = (int)out_inst;
+       }
+
+       setPin(0, 0, out);
+       //28analogWrite(servoPin, out);
+       //Serial.print("clawCommand = ");
+       //Serial.print(clawCommand);
+       //Serial.print("\t out = ");
+       //Serial.print(out);
+       //Serial.print("\t avg_pos = ");
+       //Serial.print(avg_pos);
+       //Serial.print("\t error_out = ");
+       //Serial.print(error_out);
+       //Serial.print("\n");
+       //count = 0;
+       avg_pos = 0;
+    }
+    count++;
+    // need to get desired position 
+
+/*****************************************************/
     //fromMsg.msg.pot0 = bytesRead;
     //sendMsg(fromMsg);
     msg.success = recieveMsg();
