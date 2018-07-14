1fcd7489c5ceaf5ce2fc0b03617cf9f399df79a6
simonireland <spir109@cse.unsw.edu.au>
2018-07-14 14:21:10 +1000

    Pin changes for testing on numbat arm.

--- arduino/pins.h
+++ arduino/pins.h
@@ -1,16 +1,16 @@
 // 15 means dosen't currently have an assigned pin and needs fixing
-#define FL_SPEED     2
-#define BL_SPEED     0
+#define FL_SPEED     10
+#define BL_SPEED     11
 #define FR_SPEED     3
 #define BR_SPEED     1
 #define FR_ANG       7
-#define FL_ANG       6
-#define ARM_ROT      4
-#define ARM_TOP      11
-#define ARM_BOT      10
-#define CLAW_ROT     14
+#define FL_ANG       5
+#define ARM_ROT      14
+#define ARM_TOP      0
+#define ARM_BOT      2
+#define CLAW_ROT     4
 #define LIDAR_TILT   12
-#define CLAW_GRIP    13
+#define CLAW_GRIP    6
 
 #define ARM_POT          A0
 #define LEFT_SWERVE_POT  A1
